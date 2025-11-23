bbbcbb64e17518963deda419e3f7ba11cdd9e5c9
dawei <187533154+dawei1898@users.noreply.github.com>
2025-11-23 22:25:44 +0800

    feat：ThemeProvider

---
+++ src/provider/ThemeProvider.tsx
@@ -0,0 +1,56 @@
+import React, {
+    createContext, type ReactNode, useContext, useEffect
+} from 'react';
+import {
+    type ThemeAction, type ThemeState, useThemeStore
+} from "@/stores/themeStore.ts";
+
+
+interface ThemeContextType extends ThemeState, ThemeAction {
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+
+export const useTheme = () => {
+    const content = useContext(ThemeContext);
+    if (content === undefined) {
+        throw new Error('useTheme 必须在 ThemeProvider 内使用');
+    }
+    return content;
+}
+
+
+/**
+ * 主题 Provider
+ *
+ * @param children
+ * @constructor
+ */
+export const ThemeProvider = (
+    {children}: { children: ReactNode }
+) => {
+
+    const {theme, dark, toggleTheme} = useThemeStore();
+
+
+    useEffect(() => {
+        const root = window.document.documentElement
+
+        root.classList.remove("light", "dark")
+
+        root.classList.add(theme)
+    }, [theme])
+
+
+
+    const value = {theme, dark, toggleTheme}
+
+    return (
+        <ThemeContext.Provider value={value}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
