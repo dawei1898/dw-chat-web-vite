ca6155d3fe614c18c6d242694b7f8fadaf42baa0
dawei <187533154+dawei1898@users.noreply.github.com>
2025-11-23 12:47:58 +0800

    安装 Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-
-
 function App() {
 
 
-  return (
-    <>
-      <h3>Hello World</h3>
-    </>
-  )
+    return (
+        <div className='flex min-h-svh justify-center items-center gap-4 p-4'>
+            <h3>Hello World</h3>
+            <p>This is a vite app</p>
+        </div>
+    )
 }
 
 export default App
