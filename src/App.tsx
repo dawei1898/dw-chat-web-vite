bd9c113104b6cc0e399675e284f5bc6df81b573c
dawei <187533154+dawei1898@users.noreply.github.com>
2025-11-23 12:56:40 +0800

    安装 Ant Design X

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import {Button} from "antd";
+import {Sender} from "@ant-design/x";
 
 function App() {
 
 
     return (
-        <div className='flex min-h-svh justify-center items-center gap-4 p-4'>
+        <div className='flex flex-col min-h-svh justify-center items-center gap-4 p-4'>
             <h3>Hello World</h3>
             <p>This is a vite app</p>
 
             <Button type='primary'>
                 发送
             </Button>
+
+            <div className='w-full max-w-2xl'>
+                <Sender/>
+            </div>
+
         </div>
     )
 }
